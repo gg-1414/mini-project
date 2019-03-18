130767842735511895d954685818b77d40b556a6
github username <ginalee1114@gmail.com>
2019-03-18 17:38:24 -0400

    added styling for nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import '../stylesheets/Nav.scss';
 
 const Nav = () => (
-  <div></div>
+  <nav>
+    <h1>MyWebNow</h1>
+    <div>
+      <button>Home</button>
+      <button>Why Us</button>
+      <button>Contact Us</button>
+      <button>Login</button>
+      <button>Support</button>
+    </div>
+  </nav>
 );
 
 export default Nav;

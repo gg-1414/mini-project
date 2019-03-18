6e4e1c66b6fbd724f48d688ff1aaf1bd7f4a6f06
github username <ginalee1114@gmail.com>
2019-03-18 18:58:49 -0400

    added styling for header

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import '../stylesheets/Home.scss';
 
 const Home = () => (
-  <div></div>
+  <div className="main">
+    <header>
+      <h1>Get a website in 15 minutes <span>or less</span></h1>
+      <div className="clearfix search">
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Write your domain name here.."
+        />
+        <button>Search</button>
+      </div>
+    </header>
+    <div>
+      <span>learn how we can enhance your business</span>
+      <button></button>
+    </div>
+    <div>
+
+    </div>
+    <section className="services">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </section>
+  </div>
 );
 
 export default Home;

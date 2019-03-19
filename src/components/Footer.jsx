c253e8455bdf50fa66fef7c1d4092530587245a4
github username <ginalee1114@gmail.com>
2019-03-19 01:05:21 -0400

    just had a realization that footer texts are indeed links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,35 +23,29 @@ const Footer = () => (
     </section>
     <section className="footer__company">
       <p>Company</p>
-      <p>About Us</p>
-      <p>News</p>
-      <p>FAQ</p>
-      <p>Contact Us</p>
+      <a href="#">About Us</a>
+      <a href="#">News</a>
+      <a href="#">FAQ</a>
+      <a href="#">Contact Us</a>
     </section>
     <section className="footer__services">
       <p>Services</p>
-      <p>Web Hosting</p>
-      <p>Cloud Hosting</p>
-      <p>VPS Servers</p>
-      <p>Domain Names</p>
+      <a href="#">Web Hosting</a>
+      <a href="#">Cloud Hosting</a>
+      <a href="#">VPS Servers</a>
+      <a href="#">Domain Names</a>
     </section>
     <section className="footer__other">
       <p>Other</p>
-      <p>Transfer&nbsp;domains</p>
-      <p>Customer Portal</p>
-      <p>Support Portal</p>
-      <p>Video Tutorials</p>
+      <a href="#">Transfer&nbsp;domains</a>
+      <a href="#">Customer Portal</a>
+      <a href="#">Support Portal</a>
+      <a href="#">Video Tutorials</a>
     </section>
     <section className="social">
-        <p>
-          <i className="fa fa-facebook fa-2x"></i>
-        </p>
-        <p>
-            <i className="fa fa-youtube fa-2x"></i>
-        </p>
-        <p>
-            <i className="fa fa-twitter fa-2x"></i>
-        </p>
+        <a href="#"><i className="fa fa-facebook fa-2x"></i></a>
+        <a href="#"><i className="fa fa-youtube fa-2x"></i></a>
+        <a href="#"><i className="fa fa-twitter fa-2x"></i></a>
     </section>
   </footer>
 );

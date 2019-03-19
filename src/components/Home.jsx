cd90158fef900819749199e85920efb915a78e7c
github username <ginalee1114@gmail.com>
2019-03-18 21:18:26 -0400

    style complete for services cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -27,11 +27,49 @@ const Home = () => (
     <section className="main__services">
       <h4>Our Services</h4>
       <hr/>
-      <div className="main__services-cards">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className="main__services-cards-container">
+        <div className="main__services-card">
+          <img src="/host-icon.png" alt="web host image"/>
+          <div className="main__service-cards-text">
+            <h5>web hosting</h5>
+            <i className="fa fa-arrow-right fa-1x"></i>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+        <div className="main__services-card">
+          <img src="/host-icon.png" alt="web host image"/>
+          <div className="main__service-cards-text">
+            <h5>resellers</h5>
+            <i className="fa fa-arrow-right fa-1x"></i>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+        <div className="main__services-card">
+          <img src="/host-icon.png" alt="web host image"/>
+          <div className="main__service-cards-text">
+            <h5>vps hosting</h5>
+            <i className="fa fa-arrow-right fa-1x"></i>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+        <div className="main__services-card">
+          <img src="/host-icon.png" alt="web host image"/>
+          <div className="main__service-cards-text">
+            <h5>cloud hosting</h5>
+            <i className="fa fa-arrow-right fa-1x"></i>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className="main__ready-create-contact">
+      <div>
+        <p>Are you ready?</p>
+        <p>Create an account, or contact us.</p>
+      </div>
+      <div>
+        <button>create your account</button>
+        <button>contact us</button>
       </div>
     </section>
   </div>

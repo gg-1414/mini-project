faa47ffbb8b8fbceb294b0d0bf5dad4de7ce5680
github username <ginalee1114@gmail.com>
2019-03-18 21:22:25 -0400

    added styling for light grey background skew

--- src/components/BackgroundSkew.jsx
+++ src/components/BackgroundSkew.jsx
@@ -6,7 +6,7 @@ const BackgroundSkew = () => (
     <div className="bgSkew__layer1--dark-purple"></div>
     <div className="bgSkew__layer2--purple"></div>
     <div className="bgSkew__layer3--purple-blue"></div>
-    {/*<div className="bgSkew__layer4--grey"></div>*/}
+    <div className="bgSkew__layer4--grey"></div>
   </div>
 );
 
